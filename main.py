5ff1e057633572cf609f9af81d48873f362685b2
jombi <jombi@duck.com>
2024-11-13 12:35:18 +0900

    feat :: publish, no reload

--- main.py
+++ main.py
@@ -218,4 +218,4 @@ if __name__ == "__main__":
     import uvicorn
 
     Base.metadata.create_all(bind=engine)
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
